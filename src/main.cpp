5ab57015f24190bcfda2be6129468edf1eb88183
liaoshiquan <liaoshiquan@baidu.com>
2022-10-08 21:18:32 +0800

    test serial print

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+int test_num = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  // Sets the data rate in bits per second(baud) for Serial data transmission
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
+  Serial.print(test_num++);
 }
