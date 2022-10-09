2ae6747a00ef9e05a0e91886faf07d2015bc35d3
liaoshiquan <liaoshiquan@baidu.com>
2022-10-09 10:40:53 +0800

    print some common info

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define AIO_VERSION "1.0.0"
+
+#include <Arduino.h>
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+#include "common.h"
 
 int test_num = 0;
 
@@ -6,6 +6,21 @@ void setup() {
   // put your setup code here, to run once:
   // Sets the data rate in bits per second(baud) for Serial data transmission
   Serial.begin(115200);
+
+  // https://arduinogetstarted.com/faq/what-is-serial.println-f
+  // The F() macro helps save SRAM memory by moving the constant string from SRAM to FLASH memory.  一级缓存 --> 内存 --> 硬盘 (SRAM --> DRAM --> FLASH)
+  Serial.println(F("\nAIO (All in one) version " AIO_VERSION "\n"));
+
+  // MAC ID可用作芯片唯一标识
+  Serial.print(F("ChipID(EfuseMac): "));
+  Serial.println(ESP.getEfuseMac());
+
+  Serial.print(F("CpuFreqMHz: "));
+  Serial.println(ESP.getCpuFreqMHz());
+
+  Serial.print(F("FlashChipMode: "));
+  Serial.println(ESP.getFlashChipMode());
+  Serial.println(F("FlashChipMode value: FM_QIO = 0, FM_QOUT = 1, FM_DIO = 2, FM_DOUT = 3, FM_FAST_READ = 4, FM_SLOW_READ = 5, FM_UNKNOWN = 255"));
 }
 
 void loop() {
